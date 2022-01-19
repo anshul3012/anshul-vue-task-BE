6e97b520b03cdbcde6b181305e2d9c2635de48af
Anshul Jain <anshul3012@gmail.com>
2022-01-19 18:00:00 +0530

    complete

---
+++ server.js
@@ -0,0 +1,34 @@
+'use strict';
+const Hapi = require('@hapi/hapi');
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  });
+
+  const config = {
+    cors: {
+      origin: ['*'],
+      additionalHeaders: ['cache-control', 'x-requested-with']
+    }
+  };
+
+  server.route({
+    config,
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return 'Hello World!';
+    }
+  });
+
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
